a6b451fe80b8f3a84a86eceee61a77a6946e036b
jmncamilo <jmncamilo@gmail.com>
2025-11-03 20:05:04 -0500

    feat(api/customers-enroll): add method to insert customer membership data

--- server/models/customersModel.js
+++ server/models/customersModel.js
@@ -53,37 +53,28 @@ class CustomersModel {
         return result?.affectedRows === 0 ? null : result;
     }
 
-    // Insert data into the memberships table
-    // static async insertMembership(connection = null, data) {
-    //     const executor = connection || pool;
-    //     const query = ``;
-    //     const [result] = await executor.execute(query, [data.ponerData]);
-    //     return result?.affectedRows === 0 ? null : result;
-    // }
-
-}
-
-module.exports = CustomersModel;
-
-/*
-EJEMPLO DE CÓMO ESTÁ ESTRUCTURADO EL MODELO DE LA AUTENTICACIÓN PARA GENERAR ESTANDARIZACIÓN
-class AuthModel {
-    // Get all gym and developer accounts
-    static async getAllUsers() {
-        const query = 'SELECT * FROM Gym_Dev_Accounts';
-        const [result] = await pool.query(query);
-        return result;
-    }
-
-    // Grant new user account
-    static async createNewUser(gym_name, nit, plain_pass, email, role) {
-        const query = 'INSERT INTO Gym_Dev_Accounts (gym_name, nit, hash_pass, email, role) VALUES (?, ?, ?, ?, ?)';
-        const hashedPass = await hashPassword(plain_pass);
-        const [result] = await pool.execute(query, [gym_name, nit, hashedPass, email, role]);
+    // Insert data into the customer membership table
+    static async insertCustomerMembership(connection = null, data) {
+        const executor = connection || pool;
+        const query = `INSERT INTO Customers_Memberships
+                        (customer_id_fk,
+                         membership_type,
+                         status,
+                         duration_days,
+                         start_date,
+                         end_date)
+                        VALUES (?, ?, ?, ?, ?, ?)`;
+        const [result] = await executor.execute(query, [
+            data.customer_id_fk,
+            data.membership_type,
+            data.status,
+            data.duration_days,
+            data.start_date,
+            data.end_date
+        ]);
         return result?.affectedRows === 0 ? null : result;
     }
 
 }
 
-module.exports = AuthModel;
- */
+module.exports = CustomersModel;
